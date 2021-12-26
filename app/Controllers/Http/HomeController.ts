74c4aaf1176d00977520a3a81d4e64530d617559
Wei Ji(FlySkyPie) <c445dj544@gmail.com>
2021-12-26 20:56:01 +0800

    Add migration and update home controller

--- app/Controllers/Http/HomeController.ts
+++ app/Controllers/Http/HomeController.ts
@@ -2,7 +2,15 @@ import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
 export default class HomeController {
 
-    public async index({ view }: HttpContextContract) {
+    public async index({ view, auth }: HttpContextContract) {
+        // const user = await auth.use('web').authenticate();
+        //console.log(user);
+        await auth.use('web').check()
+        //console.log(auth.use('web'));
+        if (auth.use('web').isLoggedIn) {
+            const user = await auth.use('web').authenticate();
+            return view.render('welcome', { email: user.email })
+        }
 
         return view.render('login')
     }
